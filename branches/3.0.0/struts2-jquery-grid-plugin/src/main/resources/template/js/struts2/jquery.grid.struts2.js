52a165cd49bea933047a3a19f24c9b98e994f7d1
johgep <johgep@c596f79c-574b-11de-b96a-e1c1f561d0d2>
2011-02-18 17:53:59 +0000

    Solve Issue 443: Topics are not published in Subgrids

--- branches/3.0.0/struts2-jquery-grid-plugin/src/main/resources/template/js/struts2/jquery.grid.struts2.js
+++ branches/3.0.0/struts2-jquery-grid-plugin/src/main/resources/template/js/struts2/jquery.grid.struts2.js
@@ -25,7 +25,215 @@
 
 		debugPrefix : '[struts2_jquery_grid] ',
 		lastselectedrow : '',
+		gridTopics : function($elem, o, params) {
+			var self = this;
+			if (o.onselectrowtopics || (o.editurl && o.editinline === true)) {
+				params.onSelectRow = function(id, status) {
+					var data = {};
+					data.id = id;
+					data.status = status;
+					data.grid = $elem;
+	
+					self.publishTopic($elem, o.onalw, data);
+					$.struts2_jquery.publishTopic($elem, o.onselectrowtopics,
+							data);
+					if (o.editurl && o.editinline === true) {
+						if ($.struts2_jquery_grid.lastselectedrow !== '') {
+							$elem.jqGrid('restoreRow',
+									$.struts2_jquery_grid.lastselectedrow);
+						}
+						$.struts2_jquery_grid.lastselectedrow = id;
+						var oneditbefor = null;
+						var onsuccess = null;
+						var onerror = null;
+						var onaftersave = null;
+	
+						if (o.oneibefore) {
+							oneditbefor = function() {
+								self.publishTopic($elem, o.oneibefore, data);
+							};
+						}
+						if (o.oneisuccess) {
+							onsuccess = function(response) {
+								var d = {};
+								d.response = response;
+								self.publishTopic($elem, o.oneisuccess, d);
+								if(response.status >= 400) {
+									return false;
+								}
+								else {
+									return true;
+								}
+							};
+						}
+						if (o.oneierror) {
+							onerror = function(rowid, response) {
+								var d = {};
+								d.rowid = rowid;
+								d.response = response;
+								self.publishTopic($elem, o.oneierror, d);
+							};
+						}
+						if (o.oneisave) {
+							onaftersave = function(rowid, response) {
+								var d = {};
+								d.rowid = rowid;
+								d.response = response;
+								self.publishTopic($elem, o.oneisave, d);
+							};
+						}
+	
+						//$elem.jqGrid('editRow', id, true);
+						$elem.jqGrid('editRow', id, true, oneditbefor,
+								onsuccess, null, null, onaftersave, onerror,
+								null);
+					}
+				};
+			}
+	
+			if (o.oncesuccess) {
+				params.afterSaveCell = function(rowid, cellname, value, iRow, iCol) {
+					var d = {};
+					d.rowid = rowid;
+					d.cellname = cellname;
+					d.value = value;
+					d.iRow = iRow;
+					d.iCol = iCol;
+					self.publishTopic($elem, o.oncesuccess, d);
+				};
+			}
+	
+			if (o.onceerror) {
+				params.errorCell = function(response, status) {
+					var d = {};
+					d.response = response;
+					d.status = status;
+					self.publishTopic($elem, o.onceerror, d);
+				};
+			}
+	
+			if (o.onselectalltopics) {
+				params.onSelectAll = function(ids, status) {
+					var data = {};
+					data.ids = ids;
+					data.status = status;
+					data.grid = $elem;
+	
+					self.publishTopic($elem, o.onalw, data);
+					self.publishTopic($elem, o.onselectalltopics, data);
+				};
+			}
+			if (o.onbef) {
+				params.loadBeforeSend = function(xhr) {
+	
+					var orginal = {};
+					orginal.xhr = xhr;
+	
+					self.publishTopic($elem, o.onalw, orginal);
+					self.publishTopic($elem, o.onbef, orginal);
+				};
+			}
+	
+			if (o.onpagingtopics) {
+				params.onPaging = function(pgButton) {
+	
+					var orginal = {};
+					orginal.pgButton = pgButton;
+	
+					self.publishTopic($elem, o.onalw, orginal);
+					self.publishTopic($elem, o.onpagingtopics, orginal);
+				};
+			}
+	
+			if (o.onsortcoltopics) {
+				params.onSortCol = function(index, iCol, sortorder) {
+	
+					var orginal = {};
+					orginal.index = index;
+					orginal.iCol = iCol;
+					orginal.sortorder = sortorder;
+	
+					self.publishTopic($elem, o.onalw, orginal);
+					self.publishTopic($elem, o.onsortcoltopics, orginal);
+				};
+			}
+	
+			if (o.oncellselecttopics) {
+				params.onCellSelect = function(rowid, iCol, cellcontent, e) {
+	
+					var orginal = {};
+					orginal.rowid = rowid;
+					orginal.iCol = iCol;
+					orginal.cellcontent = cellcontent;
+					orginal.e = e;
+	
+					self.publishTopic($elem, o.onalw, orginal);
+					self.publishTopic($elem, o.oncellselecttopics, orginal);
+				};
+			}
+	
+			if (o.ongridcompletetopics) {
+				params.gridComplete = function() {
+	
+					var orginal = {};
+	
+					self.publishTopic($elem, o.onalwaystopics, orginal);
+					self.publishTopic($elem, o.ongridcompletetopics, orginal);
+				};
+			}
+	
+			if (o.onfocustopics) {
+				params.beforeSelectRow = function(rowid, e) {
+	
+					var orginal = {};
+					orginal.rowid = rowid;
+					orginal.e = e;
+	
+					self.publishTopic($elem, o.onalw, orginal);
+					self.publishTopic($elem, o.onfocustopics, orginal);
+				};
+			}
+	
+			if (o.reloadtopics) {
+				$.each(o.reloadtopics.split(','), function(i, rts) {
+					$elem.subscribe(rts, '_s2j_reloadgrid', o);
+				});
+			}
+			if (!params.loadtext && self.defaults.loadingText !== null) {
+				params.loadtext = self.defaults.loadingText;
+			}
 
+			params.loadComplete = self.pubCom($elem, o.onalw, o.oncom, null,
+					null, o);
+			params.loadError = self
+					.pubErr($elem, o.onalw, o.onerr, o.errortext);
+
+			if (o.grouping) {
+				self.require("js/plugins/grid.grouping.js");
+			}
+
+			if (o.editurl) {
+				self.require("js/plugins/jquery.searchFilter.js");
+				self.require("js/plugins/grid.formedit.js");
+				if (o.editinline) {
+					self.require("js/plugins/grid.inlinedit.js");
+				}
+			}
+
+			if (o.cellurl) {
+				self.require("js/plugins/grid.celledit.js");
+			}
+
+			if (o.navigator) {
+				self.require("js/plugins/grid.formedit.js");
+			}
+
+			if (o.navigatorsearch) {
+				self.require("js/plugins/jquery.searchFilter.js");
+			}
+			
+			return params;
+		},
 		grid : function($elem, o) {
 			var self = this;
 			self.log('grid for : ' + o.id);
@@ -66,211 +274,8 @@
 				}
 			}
 
-			if (o.onselectrowtopics || (o.editurl && o.editinline === true)) {
-				params.onSelectRow = function(id, status) {
-					var data = {};
-					data.id = id;
-					data.status = status;
-					data.grid = $elem;
-
-					self.publishTopic($elem, o.onalw, data);
-					$.struts2_jquery.publishTopic($elem, o.onselectrowtopics,
-							data);
-					if (o.editurl && o.editinline === true) {
-						if ($.struts2_jquery_grid.lastselectedrow !== '') {
-							$elem.jqGrid('restoreRow',
-									$.struts2_jquery_grid.lastselectedrow);
-						}
-						$.struts2_jquery_grid.lastselectedrow = id;
-						var oneditbefor = null;
-						var onsuccess = null;
-						var onerror = null;
-						var onaftersave = null;
-
-						if (o.oneibefore) {
-							oneditbefor = function() {
-								self.publishTopic($elem, o.oneibefore, data);
-							};
-						}
-						if (o.oneisuccess) {
-							onsuccess = function(response) {
-								var d = {};
-								d.response = response;
-								self.publishTopic($elem, o.oneisuccess, d);
-								if(response.status >= 400) {
-									return false;
-								}
-								else {
-									return true;
-								}
-							};
-						}
-						if (o.oneierror) {
-							onerror = function(rowid, response) {
-								var d = {};
-								d.rowid = rowid;
-								d.response = response;
-								self.publishTopic($elem, o.oneierror, d);
-							};
-						}
-						if (o.oneisave) {
-							onaftersave = function(rowid, response) {
-								var d = {};
-								d.rowid = rowid;
-								d.response = response;
-								self.publishTopic($elem, o.oneisave, d);
-							};
-						}
-
-						//$elem.jqGrid('editRow', id, true);
-						$elem.jqGrid('editRow', id, true, oneditbefor,
-								onsuccess, null, null, onaftersave, onerror,
-								null);
-					}
-				};
-			}
-
-			if (o.oncesuccess) {
-				params.afterSaveCell = function(rowid, cellname, value, iRow, iCol) {
-					var d = {};
-					d.rowid = rowid;
-					d.cellname = cellname;
-					d.value = value;
-					d.iRow = iRow;
-					d.iCol = iCol;
-					self.publishTopic($elem, o.oncesuccess, d);
-				};
-			}
-
-			if (o.onceerror) {
-				params.errorCell = function(response, status) {
-					var d = {};
-					d.response = response;
-					d.status = status;
-					self.publishTopic($elem, o.onceerror, d);
-				};
-			}
-
-			if (o.onselectalltopics) {
-				params.onSelectAll = function(ids, status) {
-					var data = {};
-					data.ids = ids;
-					data.status = status;
-					data.grid = $elem;
-
-					self.publishTopic($elem, o.onalw, data);
-					self.publishTopic($elem, o.onselectalltopics, data);
-				};
-			}
-			if (o.onbef) {
-				params.loadBeforeSend = function(xhr) {
-
-					var orginal = {};
-					orginal.xhr = xhr;
-
-					self.publishTopic($elem, o.onalw, orginal);
-					self.publishTopic($elem, o.onbef, orginal);
-				};
-			}
-
-			if (o.onpagingtopics) {
-				params.onPaging = function(pgButton) {
-
-					var orginal = {};
-					orginal.pgButton = pgButton;
-
-					self.publishTopic($elem, o.onalw, orginal);
-					self.publishTopic($elem, o.onpagingtopics, orginal);
-				};
-			}
-
-			if (o.onsortcoltopics) {
-				params.onSortCol = function(index, iCol, sortorder) {
-
-					var orginal = {};
-					orginal.index = index;
-					orginal.iCol = iCol;
-					orginal.sortorder = sortorder;
-
-					self.publishTopic($elem, o.onalw, orginal);
-					self.publishTopic($elem, o.onsortcoltopics, orginal);
-				};
-			}
-
-			if (o.oncellselecttopics) {
-				params.onCellSelect = function(rowid, iCol, cellcontent, e) {
-
-					var orginal = {};
-					orginal.rowid = rowid;
-					orginal.iCol = iCol;
-					orginal.cellcontent = cellcontent;
-					orginal.e = e;
-
-					self.publishTopic($elem, o.onalw, orginal);
-					self.publishTopic($elem, o.oncellselecttopics, orginal);
-				};
-			}
-
-			if (o.ongridcompletetopics) {
-				params.gridComplete = function() {
-
-					var orginal = {};
-
-					self.publishTopic($elem, o.onalwaystopics, orginal);
-					self.publishTopic($elem, o.ongridcompletetopics, orginal);
-				};
-			}
-
-			if (o.onfocustopics) {
-				params.beforeSelectRow = function(rowid, e) {
-
-					var orginal = {};
-					orginal.rowid = rowid;
-					orginal.e = e;
-
-					self.publishTopic($elem, o.onalw, orginal);
-					self.publishTopic($elem, o.onfocustopics, orginal);
-				};
-			}
-
-			if (o.reloadtopics) {
-				$.each(o.reloadtopics.split(','), function(i, rts) {
-					$elem.subscribe(rts, '_s2j_reloadgrid', o);
-				});
-			}
-
-			if (!params.loadtext && self.defaults.loadingText !== null) {
-				params.loadtext = self.defaults.loadingText;
-			}
-
-			params.loadComplete = self.pubCom($elem, o.onalw, o.oncom, null,
-					null, o);
-			params.loadError = self
-					.pubErr($elem, o.onalw, o.onerr, o.errortext);
-
-			if (o.grouping) {
-				self.require("js/plugins/grid.grouping.js");
-			}
-
-			if (o.editurl) {
-				self.require("js/plugins/jquery.searchFilter.js");
-				self.require("js/plugins/grid.formedit.js");
-				if (o.editinline) {
-					self.require("js/plugins/grid.inlinedit.js");
-				}
-			}
-
-			if (o.cellurl) {
-				self.require("js/plugins/grid.celledit.js");
-			}
-
-			if (o.navigator) {
-				self.require("js/plugins/grid.formedit.js");
-			}
-
-			if (o.navigatorsearch) {
-				self.require("js/plugins/jquery.searchFilter.js");
-			}
+			params = self.gridTopics($elem, o, params);
+			
 
 			if (o.subgrid) {
 				self.require("js/plugins/grid.subgrid.js");
@@ -298,6 +303,8 @@
 						so.url = so.url + "?id=" + row_id;
 					}
 					subgrid = $(self.escId(subgridtableid));
+					so = self.gridTopics(subgrid, so, so);
+
 					subgrid.jqGrid(so);
 					if (so.navigator) {
 						var navparams = {};
