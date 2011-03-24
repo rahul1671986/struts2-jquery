1b2cb3fafa2fc0be882a67021800d324e9b179fe
johgep <johgep@c596f79c-574b-11de-b96a-e1c1f561d0d2>
2011-03-24 21:17:19 +0000

    delete branch for version 3.0.0

--- branches/3.0.0/struts2-jquery-grid-showcase/src/main/java/com/jgeppert/struts2/jquery/grid/showcase/dao/EmployeeDao.java
+++
@@ -1,6 +0,0 @@
-package com.jgeppert.struts2.jquery.grid.showcase.dao;
-
-import com.jgeppert.struts2.jquery.grid.showcase.model.Employees;
-
-public class EmployeeDao extends AbstractSimpleGenericDao<Employees, Integer> {
-}
--- branches/3.0.0/struts2-jquery-mobile-showcase/src/main/webapp/pages/echo.jsp
+++
@@ -1,2 +0,0 @@
-<%@ taglib prefix="s" uri="/struts-tags"%>
-<div>Echo : <s:property value="echo" escape="%{escape}"/></div>
